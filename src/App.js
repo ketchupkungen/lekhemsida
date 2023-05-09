3d7f1147a876423c433db3874f850a9435cbf08b
Björn O <bjorn.ohlsson.93@gmail.com>
2023-05-09 22:38:31 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="page-content">
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Mountain View</h2>
+          <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">To The Beach</h2>
+          <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Desert Destinations</h2>
+          <p className="copy">It's the desert you've always dreamed of</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Explore The Galaxy</h2>
+          <p className="copy">Seriously, straight up, just blast off into outer space today</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
+    </main>
   );
 }
 
