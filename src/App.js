bd8e11766d5d220425da5e82b52adfb6b285a98b
Björn O <bjorn.ohlsson.93@gmail.com>
2023-05-09 23:21:02 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -32,6 +32,62 @@ function App() {
           <button className="btn">Book Now</button>
         </div>
       </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Mountain View</h2>
+          <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">To The Beach</h2>
+          <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Desert Destinations</h2>
+          <p className="copy">It's the desert you've always dreamed of</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Explore The Galaxy</h2>
+          <p className="copy">Seriously, straight up, just blast off into outer space today</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Mountain View</h2>
+          <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">To The Beach</h2>
+          <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+          <button className="btn">View Trips</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Desert Destinations</h2>
+          <p className="copy">It's the desert you've always dreamed of</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="content">
+          <h2 className="title">Explore The Galaxy</h2>
+          <p className="copy">Seriously, straight up, just blast off into outer space today</p>
+          <button className="btn">Book Now</button>
+        </div>
+      </div>
     </main>
   );
 }
